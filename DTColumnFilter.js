47985a7a4293d468f414b21991eadac70d8d1fe6
Konstantin Kaluzhnikov <evilkraft@gmail.com>
2026-08-14 14:13:14 +0400

    Update DTColumnFilter.js

--- DTColumnFilter.js
+++ DTColumnFilter.js
@@ -1,53 +1,5 @@
-
-
-(function(factory){
-    if (typeof define === 'function' && define.amd) {
-        // AMD
-        define(['datatables.net'], function (dt) {
-            return factory(window, document, dt);
-        });
-    }
-    else if (typeof exports === 'object') {
-        // CommonJS
-        var cjsRequires = function (root) {
-            if (! root.DataTable) {
-                require('datatables.net')(root);
-            }
-        };
-
-        if (typeof window === 'undefined') {
-            module.exports = function (root) {
-                if (! root) {
-                    // CommonJS environments without a window global must pass a
-                    // root. This will give an error otherwise
-                    root = window;
-                }
-
-                cjsRequires(root);
-                return factory(root, root.document, root.DataTable);
-            };
-        }
-        else {
-            cjsRequires(window);
-            module.exports = factory(window, window.document, window.DataTable);
-        }
-    }
-    else {
-        // Browser
-        factory(window, document, window.DataTable);
-    }
-}(function(window, document, DataTable) {
-    'use strict';
-
-    var Dom = DataTable.Dom;
-    var Api = DataTable.Api;
-
-
-
-
-/*
-function attachColumnFilter( api ) {
-  //  const api = new DataTable.Api(settings);
+function attachColumnFilter( settings ) {
+    const api = new DataTable.Api(settings);
     const init = api.init();
 
     // noinspection JSUnresolvedReference
@@ -130,19 +82,9 @@ function attachColumnFilter( api ) {
     });
 }
 
-
-    Dom.s(document).on('init.dt', function (e, dtSettings) {
-        if (e.namespace !== 'dt') {
-            return;
-        }
-        attachColumnFilter(Api);
-    });*/
-
-    return DataTable;
-}));
-
 // Глобальное подключение для всех таблиц
-/*if (typeof DataTable !== 'undefined' && typeof DataTable.on === 'function') {
+/*
+if (typeof DataTable.on === 'function') {
     DataTable.on('init', (e, settings) => {
         attachColumnFilter(settings);
     });
@@ -157,11 +99,8 @@ function attachColumnFilter( api ) {
 
         attachColumnFilter(settings);
     };
-}*/
-
-/*
-
-$(document).on( 'init.dt', function ( e, settings ) {
-    attachColumnFilter(settings);
-});
+}
 */
+$(document).on( 'init.dt', function ( e, settings ) {
+    attachColumnFilter( settings );
+});
